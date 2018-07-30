ddcb3f3406b6fb27d11312566f5229d18566dc64
kallip <kallinpettit@gmail.com>
2018-07-30 14:31:40 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(number) 
   if number % 3 == 0 && number % 5 == 0
     returns "FizzBuzz" 
-  elsif number 3
+  elsif number % 3
     returns "Fizz"
-  else number 5
+  else number % 5
     returns "Buzz"
   end
 end
